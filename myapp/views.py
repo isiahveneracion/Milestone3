e0896c31ea73174aa8240687f7765177e15059d8
isiahveneracion <156878835+isiahveneracion@users.noreply.github.com>
2024-05-11 21:57:55 +0800

    third push- go to modals

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import StudentForm
 # Create your views here.
 
 def home(request):
+
+    if request.method =="POST":
+        form = StudentForm(request.POST)
+        if(form.is_valid()):
+            first_name = form.cleaned_data["first_name"]
+            return HttpResponse(f"Thank you , {first_name}")
+
     context = {}
     context["form"] = StudentForm
     return render(request, 'students_form.html', context)
