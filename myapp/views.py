9b8315eba23a137559fb2c2ddab37dd82c7b85cc
isiahveneracion <156878835+isiahveneracion@users.noreply.github.com>
2024-05-11 23:21:22 +0800

    4th push

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import StudentForm
+from .models import Student
 # Create your views here.
 
 def home(request):
@@ -9,6 +10,15 @@ def home(request):
         form = StudentForm(request.POST)
         if(form.is_valid()):
             first_name = form.cleaned_data["first_name"]
+            student = Student(
+                first_name = form.cleaned_data["first_name"],
+                last_name = form.cleaned_data["last_name"],
+                age = form.cleaned_data["age"],
+                email = form.cleaned_data["email"],
+                course = form.cleaned_data["course"],
+            )
+            student.save()
+
             return HttpResponse(f"Thank you , {first_name}")
 
     #GET
